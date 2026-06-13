49f78c187b7ac2161c79058673d539e1b637891b
kashvi-gandhi <kashvig2014@gmail.com>
2026-06-13 22:58:17 +0530

    added dataset

--- movies/management/commands/seed_sample_movies.py
+++ movies/management/commands/seed_sample_movies.py
@@ -1,13 +1,18 @@
-from movies.models import Theater, Seat
+from django.core.management.base import BaseCommand
+from movies.models import Theater, Seat, Movie
 
-theaters = Theater.objects.all()
-seat_numbers = ["A1", "A2", "A3", "A4", "A5"]
+class Command(BaseCommand):
+    help = 'Add seats to all existing theaters'
 
-for theater in theaters:
-    for seat_no in seat_numbers:
-        Seat.objects.get_or_create(
-            theater=theater,
-            seat_number=seat_no
-        )
+    def handle(self, *args, **kwargs):
+        theaters = Theater.objects.all()
+        seat_numbers = ["A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5"]
 
-print("Seats added successfully!")
+        for theater in theaters:
+            for seat_no in seat_numbers:
+                Seat.objects.get_or_create(
+                    theater=theater,
+                    seat_number=seat_no
+                )
+
+        self.stdout.write(self.style.SUCCESS("Seats added successfully to all theaters!"))
